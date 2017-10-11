8f5ed55c82194963e5671ff7a23bd9496f0231b6
tienvv.it <vantienvnn@gmail.com>
2017-10-11 12:00:55 +0700

    Add new driver

--- core/lib/Drupal/Core/Database/Driver/sqlsrv/Programability/ScalarValued/CONNECTION_ID.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE FUNCTION [dbo].[CONNECTION_ID]() RETURNS smallint AS
-            BEGIN
-              DECLARE @var smallint
-              SELECT @var = @@SPID
-              RETURN @Var
-            END
--- core/lib/Drupal/Core/Database/Driver/sqlsrv/Programability/ScalarValued/LPAD.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE FUNCTION [dbo].[LPAD](@str nvarchar(max), @len int, @padstr nvarchar(max)) RETURNS nvarchar(4000) AS
-            BEGIN
-	            RETURN left(@str + replicate(@padstr,@len),@len);
-            END
