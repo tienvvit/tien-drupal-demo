e856ec1b3e6116d3339c549440b712550c7b2bac
tienvv.it <vantienvnn@gmail.com>
2017-10-11 13:48:08 +0700

    Remove sql server

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Programability/ScalarValued/MD5.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE FUNCTION [dbo].[MD5](@value varchar(255)) RETURNS varchar(32) AS
-            BEGIN
-	            RETURN SUBSTRING(sys.fn_sqlvarbasetostr(HASHBYTES('MD5', @value)),3,32);
-            END
--- drivers/lib/Drupal/Driver/Database/sqlsrv/TransactionScopeOption.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\Driver\Database\sqlsrv;
-
-class TransactionScopeOption extends Enum {
-  const RequiresNew = 'RequiresNew';
-  const Supress = 'Supress';
-  const Required = 'Required';
-}
